21093a699f5d64ef7e171400d29efbb281877cc9
Jose Lopez <jose.lopez@pinnacleaerospace.com>
2023-07-14 08:57:41 -0700

    updatesv2

--- Obstacle.cs
+++ Obstacle.cs
@@ -1,84 +1,114 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
-class Program
+namespace ObstacleGame
 {
-    static void Main()
+    public class Obstacle
     {
-        int gridSize = 30;
-        char[,] grid = new char[gridSize, gridSize];
+        public int PositionX1 { get; }
+        public int PositionX2 { get; }
+        public int PositionY1 { get; }
+        public int PositionY2 { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public int HitPoints { get; private set; } 
 
-        Random random = new Random();
+    public Obstacle(int positionX1, int positionY1, int width, int height)
+    {
+        PositionX1 = positionX1;
+        PositionY1 = positionY1;
+        Width = width;
+        Height = height;
 
-        void InitializeGrid()
+        PositionX2 = positionX1 + width - 1;
+        PositionY2 = positionY1 + height - 1;
+
+        HitPoints = 5; 
+    }
+    public void HitObstacle()
+    {
+        if (HitPoints > 0)
         {
-            for (int i = 0; i < gridSize; i++)
+            HitPoints--;
+            Console.WriteLine("Obstacle hit! Remaining hit points: " + HitPoints);
+        }
+        else
+        {
+            Console.WriteLine("Obstacle has no more hit points!");
+        }
+    }
+
+
+    }
+//test
+    public class Stage
+    {
+        private const int GridSize = 30;
+        private const int MinObstacleSize = 2;
+        private const int MaxObstacleSize = 3;
+
+        private readonly Obstacle[,] grid;
+        private readonly Random random;
+        private readonly List<Obstacle> obstacles; // New list field
+
+        public Stage()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            grid = new Obstacle[GridSize, GridSize];
+            random = new Random();
+            obstacles = new List<Obstacle>(); // Initialize the list
+        }
+
+        public void GenerateObstacles(int obstacleCount)
+        {
+            for (int i = 0; i < obstacleCount; i++)
             {
-                for (int j = 0; j < gridSize; j++)
+                int width = random.Next(MinObstacleSize, MaxObstacleSize + 1);
+                int height = random.Next(MinObstacleSize, MaxObstacleSize + 1);
+
+                int positionX = random.Next(0, GridSize - width + 1);
+                int positionY = random.Next(0, GridSize - height + 1);
+
+                var obstacle = new Obstacle(positionX, positionY, width, height);
+                PlaceObstacle(obstacle);
+                obstacles.Add(obstacle); // Add the obstacle to the list
+            }
+        }
+
+        private void PlaceObstacle(Obstacle obstacle)
+        {
+            for (int i = obstacle.PositionX1; i <= obstacle.PositionX2; i++)
+            {
+                for (int j = obstacle.PositionY1; j <= obstacle.PositionY2; j++)
                 {
-                    grid[i, j] = '.';
+                    grid[i, j] = obstacle;
                 }
             }
         }
 
-        void AddObstacle(int x, int y, int width, int height)
+        public void PrintStage()
         {
-            for (int i = 0; i < width; i++)
+            for (int i = 0; i < GridSize; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = 0; j < GridSize; j++)
                 {
-                    grid[y + j, x + i] = '■';
-                }
-            }
-        }  
-
-        void DisplayGrid()
-        {
-            for (int i = 0; i < gridSize; i++)
-            {
-                for (int j = 0; j < gridSize; j++)
-                {
-                    Console.Write(grid[i, j] + " ");
+                    if (grid[i, j] != null)
+                    {
+                        Console.Write("⬜"); // Full block character for obstacle
+                    }
+                    else
+                    {
+                        Console.Write("⬛"); // Empty space
+                    }
                 }
                 Console.WriteLine();
             }
         }
 
-        void RefreshGrid()
+        public List<Obstacle> GetObstacles()
         {
-            InitializeGrid();
-
-            // Generate random 2x2 obstacles
-            for (int k = 0; k < 10; k++)
-            {
-                int x = random.Next(4, gridSize - 4); // Avoid corners
-                int y = random.Next(4, gridSize - 4); // Avoid corners
-                AddObstacle(x, y, 2, 2);
-            }
-
-            // Generate random 3x2 obstacles
-            for (int k = 0; k < 10; k++)
-            {
-                int x = random.Next(4, gridSize - 4); // Avoid corners
-                int y = random.Next(4, gridSize - 4); // Avoid corners
-                AddObstacle(x, y, 3, 2);
-            }
-
-            Console.Clear();
-            DisplayGrid();
-        }
-
-        // Initialize the grid
-        InitializeGrid();
-
-        // Display the initial grid
-        while (true)
-        {
-            Console.WriteLine("Press any key to refresh the grid...");
-            Console.ReadKey(true);
-
-            // Refresh the grid
-            RefreshGrid();
-
+            return obstacles;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
-namespace Granata
+using System.Collections.Generic;
+using System.Text;
+namespace ObstacleGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Stage stage = new Stage();
+            stage.GenerateObstacles(10); // Generate 10 obstacles
+            stage.PrintStage(); // Print the grid with obstacles
+            List<Obstacle> obstacles = stage.GetObstacles();
+            foreach (Obstacle obstacle in obstacles)
+            {
+                Console.WriteLine($"Obstacle at position = (X1 = {obstacle.PositionX1}, Y1 = {obstacle.PositionY1}, X2 = {obstacle.PositionX2}, Y2 = {obstacle.PositionY2}) | HP: {obstacle.HitPoints}, H = {obstacle.Width}, W = {obstacle.Height} ");
+            }
+            
         }
     }
 }
