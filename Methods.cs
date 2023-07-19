83c2d7c4dc3d86df7ebcbbc8b9490c46cfc4aeb1
Sebastian Moreno <sebastian.moreno@pinnacleaerospace.com>
2023-07-19 08:13:49 -0700

    adding mines

--- Methods.cs
+++ Methods.cs
@@ -8,41 +8,45 @@ namespace Granata
         {
             Stage.RenderGrid();
             Stage.players[playerN].ShowInventory(playerN);
-            if (Stage.players[playerN].HP<=0)
+            if (Stage.players[playerN].HP <= 0)
             {
                 return;
             }
-            
+
             bool done = false;
             int actionCount = 0;
-            int maxActionCount = 4;
+            int maxActionCount = 10;
             //By default the player can move up to 4 times, and throw only once.
             while (!done)
-            {   
+            {
+                if (Stage.players[playerN].HP <= 0)
+            {
+                return;
+            }
                 Console.BackgroundColor = ConsoleColor.DarkBlue;
                 Console.ForegroundColor = ConsoleColor.White;
-                System.Console.Write($"Player {playerN+1} What do you want to do? WASD to Move(You have {maxActionCount-actionCount} move actions left to end turn)\nNumberpad numbers to Throw.............................................................");
+                System.Console.Write($"Player {playerN + 1} What do you want to do? WASD to Move(You have {maxActionCount - actionCount} move actions left to end turn)\nNumberpad numbers to Throw.............................................................");
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.Black;
+                //Console.ForegroundColor = ConsoleColor.Black;
                 System.Console.WriteLine("\n");
                 char input = Console.ReadKey().KeyChar;
                 System.Console.WriteLine();
-                input=char.ToUpper(input);
+                input = char.ToUpper(input);
                 switch (input)
-                {  
+                {
                     //For moving:
                     case 'W':
                     case 'A':
                     case 'S':
                     case 'D':
-                        Stage.players[playerN].Move(input,playerN);
+                        Stage.players[playerN].Move(input, playerN);
                         if (actionCount >= maxActionCount)
                         {
                             done = true;
                         }
                         actionCount += 1;
                         break;
-                    
+
                     //For throwing projectile:
                     case '7':
                     case '8':
@@ -53,28 +57,40 @@ namespace Granata
                     case '1':
                     case '2':
                     case '3':
-                        int intInput=input - '0';
+                        int intInput = input - '0';
                         Stage.players[playerN].ShowInventory(playerN);
                         System.Console.Write("Select a projectile (1 rock, 2 grenade, 3 mine): ");
-                        string projType = Console.ReadLine(); //ask user which type of projectile want 1,2,3
-                        if (Stage.players[playerN].CheckProjectileAvailible(projType,playerN))
+
+                        string projType = StringIntInput(1, 3);
+                        Player.Sound("Ok.wav");
+                        if (Stage.players[playerN].CheckProjectileAvailible(projType, playerN))
                         {
-                            var position=Stage.players[playerN].Position;
-                            bool playerCollision=false;
-                            int[] pos = new int[] { position[0], position[1]};
-                            
-                            Stage.actualProjectile = GetProjectile(projType, playerN, intInput); 
-                            for (int i = 0;i<Stage.actualProjectile.Frames;i++)
+                            var position = Stage.players[playerN].Position;
+                            bool playerCollision = false;
+                            int[] pos = new int[] { position[0], position[1] };
+                            int[] posProj = { Stage.players[playerN].Position[0], Stage.players[playerN].Position[1] };
+                            Stage.actualProjectile = GetProjectile(projType, posProj, intInput);
+                            for (int i = 0; i < Stage.actualProjectile.Frames; i++)
                             {
-                                Stage.players[playerN].Throw(intInput,projType,playerN);
-                                (intInput,playerCollision)=Player.Collision(intInput);
+                                Stage.players[playerN].Throw(intInput, projType, playerN);
+                                (intInput, playerCollision) = Player.Collision(intInput);
                                 if (playerCollision) break;
                             }
-                            Stage.players[playerN].grenadeImpact();
-                            //Player.Sound(Stage.actualProjectile.Tipo);
+
+                            if (!playerCollision && Stage.players[playerN].grenadeImpact())
+                                Player.Sound("Bum.wav");
+                            int[] posMines = {Stage.actualProjectile.ProjectilePosition[0], Stage.actualProjectile.ProjectilePosition[1]};
+                            if (!playerCollision && Stage.players[playerN].plantMine(posMines))
+                            {
+                                Player.Sound("Planted.wav");
+                                System.Console.WriteLine("MINA");
+                            }
+                                
+                                //Player.Sound("Bum.wav");
                             
-                        }  
-                        Stage.actualProjectile=null;  
+
+                        }
+                        Stage.actualProjectile = null;
                         done = true;
                         break;
                     default:
@@ -84,59 +100,77 @@ namespace Granata
             }
         }
 
-        internal static Projectile GetProjectile(string type, int playerN, int dir)
-        {
-
-            int[] pos = {Stage.players[playerN].Position[0], Stage.players[playerN].Position[1]};
-            switch(type)
+        public static Projectile GetProjectile(string type,int[] pos, int dir)
+        {            
+            switch (type)
             {
                 case "1":
-                return new Projectile("1", pos, dir, 25, 30, 0, "⚾");
-                break;
+                    return new Projectile("1", pos, dir, 25, 30, 0, "⚾");
+                    break;
 
                 case "2":
-                return new Projectile("2", pos, dir, 75, 25, 25, "💣");
-                break;
+                    return new Projectile("2", pos, dir, 75, 25, 25, "💣");
+                    break;
 
                 case "3":
-                return new Projectile("3", pos, dir, 75, 25, 25, "🧨");
-                break;
+                    return new Projectile("3", pos, dir, 100, 25, 25, "🧨");
+                    break;
 
                 default:
                     System.Console.WriteLine("not a valid type!");
                     return null;
                     break;
-            }            
+            }
         }
         internal static void supplyProjectiles()
-        {            
+        {
             System.Console.WriteLine("Supplying projectiles");
             foreach (var player in Stage.players)
             {
                 player.Refill();
-            }            
+            }
         }
-        internal static void ValidateConfigInput(ref int value,int defaultValue,int maxValue)
+        internal static void ValidateConfigInput(ref int value, int defaultValue, int maxValue)
         {
-            bool done=false;
-            while (!done){
-                var input=Console.ReadLine();
+            bool done = false;
+            while (!done)
+            {
+                var input = Console.ReadLine();
                 if (input == "") //if input is empty we take the default value
                 {
                     value = defaultValue;
                     break;
                 }
-                done=int.TryParse(input,out value);
+                done = int.TryParse(input, out value);
                 if (!done)
                 {
                     System.Console.WriteLine("Please input an integer value!");
                     continue;
                 }
-                if (value>maxValue)
+                if (value > maxValue)
                 {
-                    done=false;
+                    done = false;
                     System.Console.WriteLine($"Max value is {maxValue}!");
-                }  
+                }
+            }
+        }
+
+
+        internal static string StringIntInput(int minValue, int maxValue)
+        {
+            //validates that input int string eg "2" is within values of minValue and maxValue            
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out int result) && result >= minValue && result <= maxValue)
+                {
+                    return input;
+                }
+                else
+                {
+                    System.Console.WriteLine($"Select a value between {minValue} and {maxValue}");
+                }
+
             }
         }
     }
